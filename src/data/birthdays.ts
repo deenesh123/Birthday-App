94c8a032f5e400ba0655a82082c0998706bc3c65
deenesh123 <89989081+deenesh123@users.noreply.github.com>
2024-06-01 11:34:29 +0530

    Create birthdays.ts

---
+++ src/data/birthdays.ts
@@ -0,0 +1,13 @@
+import { Birthday } from '../models/Birthday';
+
+const birthdays: Birthday[] = [
+  {
+    name: 'Anna Apple',
+    profession: 'American writer',
+    date: new Date(2023, 1, 3), // February 3rd
+    isFavourite: false,
+  },
+  // ... add more birthdays
+];
+
+export default birthdays;
